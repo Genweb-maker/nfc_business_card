c2f5976f340f5bd833cd3b530f71f6b63bfbbc2c
Genweb-maker <genweb037@gmail.com>
2025-06-08 18:40:34 +0530

    removed the frontend

--- frontend/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Barrel export file for all components
-export { default as AuthPage } from './AuthPage';
-export { default as Dashboard } from './Dashboard';
-export { default as ProfilePage } from './ProfilePage';
-export { default as SharePage } from './SharePage';
-export { default as ConnectionsPage } from './ConnectionsPage';
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as Navbar } from './Navbar';
-export { default as Toast } from './Toast'; 
